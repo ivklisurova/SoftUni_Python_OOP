46b6a0987a4584d1d1de86cff7a42b8ec471f734
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2020-07-28 22:23:13 +0300

    iterators and generators

--- itterators_and_generators/possible_permutations.py
+++ itterators_and_generators/possible_permutations.py
@@ -1,5 +1,13 @@
-def possible_permutations():
-    pass
+from itertools import permutations
+
+
+def possible_permutations(l):
+    a = list(permutations(l))
+    for i in a:
+        yield list(i)
 
 
 [print(n) for n in possible_permutations([1, 2, 3])]
+
+# for n in possible_permutations([1, 2, 3]):
+#     print(n)
